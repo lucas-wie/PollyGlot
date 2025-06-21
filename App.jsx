d6792ef456fd2d1b3f89888bd3372fec9e2c32e5
Lucas <lucaswie@outlook.com>
2025-06-20 21:55:01 -0300

    improve main screen style

--- App.jsx
+++ App.jsx
@@ -29,6 +29,21 @@ export default function App() {
   );
 }
 
+function Header() {
+  return (
+    <div className="banner">
+      <img src={worldmap} alt="World Map" className="worldmap" />
+      <div className="parrot-section">
+        <img src={parrot} alt="Parrot" className="parrot" />
+        <div>
+          <h1 className="">PollyGlot</h1>
+          <h3>Perfect Translation Every Time</h3>
+        </div>
+      </div>
+    </div>
+  ) 
+}
+
 function MainScreen({ setText, setLanguage }) {
   const navigate = useNavigate();
   const [inputText, updateText] = useState("");
@@ -43,56 +58,60 @@ function MainScreen({ setText, setLanguage }) {
   return (
     <div className="main-div">
       <Header />
-      <div className="max-w-md mx-auto mt-6 p-4 border rounded-xl shadow-md">
-        <label className="font-bold text-blue-800">Text to translate 👇</label>
-        <textarea
-          value={inputText}
-          onChange={(e) => updateText(e.target.value)}
-          className="w-full mt-2 p-2 border rounded resize-none"
-          rows={3}
-          placeholder="How are you?"
-        />
-
-        <label className="font-bold text-blue-800 mt-4 block">Select language 👇</label>
-        <div className="mt-2 space-y-2">
-          <label className="flex items-center">
-            <input
-              type="radio"
-              name="language"
-              value="fr"
-              checked={selectedLang === "fr"}
-              onChange={() => setSelectedLang("fr")}
-            />
-            <img src={frFlag} alt="French" className="h-5 ml-2" /> French
-          </label>
-          <label className="flex items-center">
-            <input
-              type="radio"
-              name="language"
-              value="es"
-              checked={selectedLang === "es"}
-              onChange={() => setSelectedLang("es")}
-            />
-            <img src={spFlag} alt="Spanish" className="h-5 ml-2" /> Spanish
-          </label>
-          <label className="flex items-center">
-            <input
-              type="radio"
-              name="language"
-              value="jp"
-              checked={selectedLang === "jp"}
-              onChange={() => setSelectedLang("jp")}
-            />
-            <img src={jpnFlag} alt="Japanese" className="h-5 ml-2" /> Japanese
-          </label>
+      <div className="main-div-translate-area">
+        <div className="text-to-translate-area">
+          <label className="font-bold text-blue-800">Text to translate 👇</label>
+          <textarea
+            value={inputText}
+            onChange={(e) => updateText(e.target.value)}
+            className="w-full mt-2 p-2 border rounded resize-none"
+            rows={3}
+            placeholder="How are you?"
+          />
         </div>
 
-        <button
-          onClick={handleTranslate}
-          className="mt-4 w-full bg-blue-700 text-white py-2 rounded hover:bg-blue-800"
-        >
-          Translate
-        </button>
+        <div className="language-selection-area">
+          <label className="language-selection-area-label">Select language 👇</label>
+          <div className="language-selection-area-options">
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="language"
+                value="fr"
+                checked={selectedLang === "fr"}
+                onChange={() => setSelectedLang("fr")}
+              />
+              <img src={frFlag} alt="French" className="h-5 ml-2" /> French
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="language"
+                value="es"
+                checked={selectedLang === "es"}
+                onChange={() => setSelectedLang("es")}
+              />
+              <img src={spFlag} alt="Spanish" className="h-5 ml-2" /> Spanish
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="language"
+                value="jp"
+                checked={selectedLang === "jp"}
+                onChange={() => setSelectedLang("jp")}
+              />
+              <img src={jpnFlag} alt="Japanese" className="h-5 ml-2" /> Japanese
+            </label>
+          </div>
+
+          <button
+            onClick={handleTranslate}
+            className="mt-4 w-full bg-blue-700 text-white py-2 rounded hover:bg-blue-800"
+          >
+            Translate
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -134,19 +153,4 @@ function TranslatedScreen({ text, translationResponse }) {
       </div>
     </div>
   );
-}
-
-function Header() {
-  return (
-    <div className="banner">
-      <img src={worldmap} alt="World Map" className="worldmap" />
-      <div className="parrot-section">
-        <img src={parrot} alt="Parrot" className="parrot" />
-        <div>
-          <h1 className="">PollyGlot</h1>
-          <h3>Perfect Translation Every Time</h3>
-        </div>
-      </div>
-    </div>
-  ) 
 }
